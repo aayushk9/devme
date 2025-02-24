82b1a168eeb8cc517e5f27fb25ae3bb5021a557d
aayushk9 <ayushkokate1@gmail.com>
2025-02-24 22:54:05 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    return (
+      <div className='bg-black text-white min-h-screen w-full'>
+        <h1>HELLO</h1>
+      </div>
+    )
+}
+
+export default App
