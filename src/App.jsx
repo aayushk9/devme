f92cbcf872724b415f8c5550165526aab6594be2
aayushk9 <ayushkokate1@gmail.com>
2025-02-25 19:04:11 +0530

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
+import {Router, BrowserRouter} from 'react-router-dom' 
+import { Intro } from './components/Intro'
+import { About} from './components/About'
+import { Edu } from './components/Edu'   
+import { Projects } from './components/Projects'
+import { Blogs } from './components/Blogs' 
+import { Skills } from './components/Skills'
 import './App.css'
+
 
 function App() {
     return (
-      <div className='bg-black text-white min-h-screen w-full'>
-        <h1>HELLO</h1>
-      </div>
+       <div>
+        <Intro/>
+        <About/>
+        <Edu/>
+        <Projects/>
+        <Blogs/>
+        <Skills/>
+       </div>
     )
 }
 
 export default App
+  
